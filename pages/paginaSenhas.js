4bb2754094f95351b0cabaf0f09eaab9704d465a
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-03-21 13:49:04 -0300

    vbvb

--- pages/paginaSenhas.js
+++ pages/paginaSenhas.js
@@ -1,13 +1,50 @@
-import { View, StyleSheet, Text} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Armazenamento from '../hooks/bancoTokens';
+import { useState, useEffect } from 'react';
+import { CaixaToken } from '../components/tokenView';
+import { useIsFocused } from '@react-navigation/native';
 
 export function PaginaSenhas() {
+    const { obterItem, removerItem } = Armazenamento();
+    const [listaTokens, defListaTokens] = useState([]);
+    const telaAtiva = useIsFocused();
+
+    useEffect(() => {
+        async function carregaTokens() {
+            const tokens = await obterItem("@token");
+            defListaTokens(tokens);
+        }
+        carregaTokens()
+    }, [telaAtiva]);
+
+
+    async function carregaTokens() {
+        const tokens = await obterItem("@token");
+        defListaTokens(tokens);
+    }
+    
+    async function deletarToken(item) {
+        const tokens  = await removerItem("@token", item)
+        defListaTokens(tokens)
+    };
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={ESTILO.header}>
                 <Text style={ESTILO.title}>
                     Minhas senhas
                 </Text>
+            </View>
+            <View style={ESTILO.content}>
+                <FlatList
+                    style={{ flex: 1, paddingTop: 14, }}
+                    data={listaTokens}
+                    keyExtractor={(item) => String(item)}
+                    renderItem={({ item }) => <CaixaToken
+                        data={item}
+                        removePassword={() => deletarToken(item)}
+                    />}
+                />
             </View>
         </SafeAreaView>
     )
@@ -23,6 +60,11 @@ const ESTILO = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         color: "#FFF"
+    },
+    content: {
+        flex: 1,
+        paddingLeft: 14,
+        paddingRight: 14,
     }
 
 })
